e97232e9b974cb15d84060d571d41db7de714cb7
LikaGreb <114307021+LikaGreb@users.noreply.github.com>
2022-12-21 01:00:35 +0200

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { AuthComponent } from './components/auth/auth.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app-lika';
-  text = "If you are new to Angular, you might want to start with Try it now!, which introduces the essentials of Angular in the context of a ready-made basic online store app for you to examine and modify. This standalone tutorial takes advantage of the interactive StackBlitz environment for online development. You don't need to set up your local environment until you're ready.";
+  @Input() logout = false;
+  isAuth = !!localStorage.getItem('token');
+  @ViewChild(AuthComponent)
+  viewChild!: AuthComponent;
+  ngOnInit() {}
+  ngAfterContentChecked() {
+    if (this.viewChild) {
+      if (localStorage.getItem('activeID')) {
+        this.isAuth = true;
+        return;
+      }
+    }
+    if (this.logout) {
+      this.isAuth = false;
+    }
+  }
 }
